324fb24bcaf93b55fbdc5e5b0ec8686ef969cf42
Desecho <desecho@gmail.com>
2013-08-15 16:55:31 +0400

    changes

--- schedule_project/schedule/models.py
+++ schedule_project/schedule/models.py
@@ -2,7 +2,21 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.conf import settings
+import fields
 
+DAYS_OF_THE_WEEK = (
+    (0, u'Понедельник'),
+    (1, u'Вторник'),
+    (2, u'Среда'),
+    (3, u'Четверг'),
+    (4, u'Пятница'),
+    (5, u'Суббота'),
+    (6, u'Воскресенье'),
+)
+
+def get_weekday(n):
+    weekdays = {x[0]: x[1] for x in DAYS_OF_THE_WEEK}
+    return weekdays[n]
 
 class Subject(models.Model):
     name = models.CharField('название', max_length=255)
@@ -16,15 +30,18 @@ class Subject(models.Model):
 
 
 class Teacher(models.Model):
-    name = models.CharField('имя', max_length=255)
+    user = models.OneToOneField(User, verbose_name='учитель')
     subjects = models.ManyToManyField(Subject, verbose_name='предметы')
 
     class Meta:
         verbose_name = 'учитель'
         verbose_name_plural = 'учителя'
 
+    def get_name(self):
+        return self.user.get_full_name()
+
     def __unicode__(self):
-        return self.name
+        return self.get_name()
 
 
 class Student(models.Model):
@@ -58,8 +75,11 @@ class Room(models.Model):
         verbose_name = 'кабинет'
         verbose_name_plural = 'кабинеты'
 
+    def get_full_name(self):
+        return '%s - %s' % (self.office.name, self.name)
+
     def __unicode__(self):
-        return self.name
+        return self.get_full_name()
 
 
 class GroupType(models.Model):
@@ -85,14 +105,14 @@ class ScheduleSet(models.Model):
         verbose_name_plural = 'расписание установленное'
 
     def __unicode__(self):
-        return self.teacher.name + ' ' + self.room.name
-
+        return '%s - %s - %s' % (self.date.strftime(settings.DATETIME_FORMAT), self.teacher.get_name(), self.room.get_full_name())
 
 class ScheduleRegular(models.Model):
     teacher = models.ForeignKey(Teacher, verbose_name='учитель')
     room = models.ForeignKey(Room, verbose_name='кабинет')
     group_type = models.ForeignKey(GroupType, verbose_name='тип группы')
-    date = models.DateTimeField('дата/время')
+    weekday = fields.DayOfTheWeekField('день недели')
+    time = models.TimeField('время')
     students = models.ManyToManyField(Student, verbose_name='ученики')
 
     class Meta:
@@ -100,5 +120,18 @@ class ScheduleRegular(models.Model):
         verbose_name_plural = 'расписание постоянное'
 
     def __unicode__(self):
-        return self.teacher.name + ' ' + self.room.name
+        return '%s %s - %s' % (get_weekday(self.weekday), self.time.strftime(settings.TIME_FORMAT), self.teacher.get_name())
+
+
+class FreeTime(models.Model):
+    teacher = models.ForeignKey(Teacher, verbose_name='учитель')
+    weekday = fields.DayOfTheWeekField('день недели')
+    time = models.TimeField('время')
+
+    class Meta:
+        verbose_name = 'свободное время'
+        verbose_name_plural = 'свободное время'
+
+    def __unicode__(self):
+        return '%s %s - %s' % (get_weekday(self.weekday), self.time.strftime(settings.TIME_FORMAT), self.teacher.get_name())
 
--- schedule_project/schedule/views.py
+++ schedule_project/schedule/views.py
@@ -3,7 +3,7 @@
 import json
 from django.http import HttpResponse
 from django.shortcuts import redirect
-from schedule.models import Office, Room, ScheduleSet
+from schedule.models import Office, Room, ScheduleSet, ScheduleRegular
 from annoying.decorators import ajax_request, render_to
 from django.contrib.auth import logout
 from django.contrib.auth.decorators import login_required
@@ -23,10 +23,12 @@ def logout_view(request):
 def home(request):
 
     def get_start_date():
+        'Returns the datetime of Monday of the current week'
         today = date.today()
         return today - timedelta(days=today.weekday())
 
     def get_date_range():
+        'Returns a list of successive datetimes starting from Monday of the current week till Sunday'
         def daterange():
             dates = []
             for n in range(int ((end_date - start_date).days)):
@@ -36,6 +38,9 @@ def home(request):
         return daterange()
 
     def get_offices():
+        '''
+            Returns a list of dict containing office name and rooms
+        '''
         output = []
         offices = Office.objects.all()
         for office in offices:
@@ -43,7 +48,24 @@ def home(request):
             output.append({'name': office.name, 'rooms': rooms})
         return output
 
+    def get_rooms():
+        '''
+            Returns a json with a dict {room_id: room_name}
+        '''
+        output = {}
+        rooms = Room.objects.all()
+        for room in rooms:
+            output[room.pk] = room.get_full_name()
+        return json.dumps(output)
+
     def get_hours_ranges():
+        '''
+        Returns rows with working hours in a row
+        List of 3 lists of 4 ints.
+        Example:
+        [[8, 9, 10, 11], [12, 13, 14, 15], [16, 17, 18, 19]]
+
+        '''
         def chunks(l, n):
             'Yield successive n-sized chunks from l.'
             for i in xrange(0, len(l), n):
@@ -51,20 +73,41 @@ def home(request):
         hours = range(settings.WORK_TIME[0], settings.WORK_TIME[1] + 1)
         return list(chunks(hours, 4))
 
-    def get_schedules():
-        output = []
-        schedules = ScheduleSet.objects.filter(date__gte=start_date)
+    def get_schedule(schedule_type):
+        '''
+            Returns a json of a dict of set schedule starting from Monday of the current week
+        '''
+        output = {}
+        if schedule_type == 'set':
+            schedules = ScheduleSet.objects.filter(date__gte=start_date)
+        elif schedule_type == 'regular':
+            schedules = ScheduleRegular.objects.all()
+
         for schedule in schedules:
-            code = '%d_%s' % (schedule.room.pk, schedule.date.strftime('%d%m%Y_%H'))
-            output.append(code)
+            if schedule_type == 'set':
+                date = schedule.date
+            elif schedule_type == 'regular':
+                date = start_date + timedelta(schedule.weekday)
+                date = datetime.combine(date, schedule.time)
+            code = '%d_%s' % (schedule.room.pk, date.strftime('%d%m%Y_%H'))
+            output[code] = {
+                'teacher': schedule.teacher.get_name(),
+                'room': schedule.room.get_full_name(),
+                'group_type': schedule.group_type.name,
+                'students': [student.name for student in schedule.students.all()],
+            }
         return json.dumps(output)
 
     hours = get_hours_ranges()
     start_date = get_start_date()
-    dates = get_date_range()
-    offices = get_offices()
+    schedule_set = get_schedule('set')
+    schedule_regular = get_schedule('regular')
 
-    schedules = get_schedules()
-
-
-    return {'hours': hours, 'dates': dates, 'offices': offices, 'schedules': schedules}
+    return {
+        'hours': hours,
+        'dates': get_date_range(),
+        'offices': get_offices(),
+        'rooms': get_rooms(),
+        'schedule_set': schedule_set,
+        'schedule_regular': schedule_regular,
+    }
